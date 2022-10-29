024b68250e92e675741ffefa121f269dd83310fe
Syphdias <syphdias+git@gmail.com>
2022-10-29 16:10:01 +0200

    Rewrite print statement

--- i3nodes.py
+++ i3nodes.py
@@ -3,10 +3,8 @@ from i3ipc import Connection
 
 
 def print_or_nodes(o, depth=0):
-    if o.name:
-        print(f"{'  '*depth}{o.type}: {o.name} ({o.layout})")
-    else:
-        print(f"{'  '*depth}{o.type}:  ({o.layout})")
+    name = o.name or ""
+    print(f"{'  '*depth}{o.type}: {name} ({o.layout})")
     if o.nodes:
         for n in o.nodes:
             print_or_nodes(n, depth + 1)
